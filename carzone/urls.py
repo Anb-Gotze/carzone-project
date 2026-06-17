22bcf48f399e2edf90a8d0bc7b22900e52b15833
Anonymous0033 <abakwumegodwin1@gmail.com>
2026-06-17 14:19:37 +0100

    fix: force django to serve media files in production

--- carzone/urls.py
+++ carzone/urls.py
@@ -12,5 +12,6 @@ urlpatterns = [
     path('contacts/', include('contacts.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# This forces Django to serve media and static files even when DEBUG is False on Render
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
