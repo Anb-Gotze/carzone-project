dd3b765e272b322ca01d1f8c3db6e9bbb972b89d
Anonymous0033 <abakwumegodwin1@gmail.com>
2026-06-17 13:53:23 +0100

    fix: update allowed hosts for render

--- carzone/settings.py
+++ carzone/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-#3&3^@j&-)%53m-6sjkl@$s3all-es4*(cq7-sd^imv^&p(!wn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['carzone-project-1.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
