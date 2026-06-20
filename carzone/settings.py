9e8e6db3e6d7a3850fd747e031a942e253778418
Anonymous0033 <abakwumegodwin1@gmail.com>
2026-06-20 12:18:10 +0100

    security: disable debug mode for production

--- carzone/settings.py
+++ carzone/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True').lower() == 'true' and not os.environ.get('RENDER')
 
 ALLOWED_HOSTS = [
     'carzone-project-4.onrender.com',
